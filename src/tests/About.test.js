532b4ed69aec5931073eba7c97f13f0f154bedfc
Gustavo <gustavodarocha241@gmail.com>
2022-08-08 18:56:08 -0300

    requisito 1 e 2 completos

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,27 @@
-test('sksksks', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from './RenderWithRouter';
+import { About } from '../pages';
+
+describe('Testa a rota /about ', () => {
+  it('Teste se a página contém as informações sobre a Pokédex', () => {
+    renderWithRouter(<About />);
+    const textAbout = screen.getByText(/About pokédex/i);
+
+    expect(textAbout).toBeInTheDocument();
+
+    const text1 = screen.getByText(/This Application/i);
+    const text2 = screen.getByText(/One can filter/i);
+    expect(text1).toBeInTheDocument();
+    expect(text2).toBeInTheDocument();
+  });
+
+  it('Testa se imagem da rota /about tem o atributo SRC esperado', () => {
+    renderWithRouter(<About />);
+
+    const imgPokedex = screen.getByAltText(/Pokédex/i);
+    const URL = 'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png';
+
+    expect(imgPokedex).toHaveProperty('src', `${URL}`);
+  });
+});
--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,40 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import renderWithRouter from './RenderWithRouter';
+import App from '../App';
+
+describe('Testando a navegaçao no app', () => {
+  it('testa se é redirecionado para "/" ao clicar em home', () => {
+    renderWithRouter(<App />);
+    const home = screen.getByText(/home/i);
+
+    expect(home).toBeInTheDocument();
+
+    userEvent.click(home);
+    const text = screen.getByRole('heading', { name: /Encountered pokémons/i });
+    expect(text).toBeInTheDocument();
+  });
+
+  it('testa se é redirecionado para "/about" ao clicar em About', () => {
+    renderWithRouter(<App />);
+    const about = screen.getByText(/about/i);
+
+    expect(about).toBeInTheDocument();
+
+    userEvent.click(about);
+    const aboutText = screen.getByRole('heading', { name: /about Pokédex/i });
+    expect(aboutText).toBeInTheDocument();
+  });
+
+  it('testa se é redirecionado para "/favorites" ao clicar em Favorite', () => {
+    renderWithRouter(<App />);
+    const favorite = screen.getByText(/favorite pokémons/i);
+
+    expect(favorite).toBeInTheDocument();
+
+    userEvent.click(favorite);
+    const favoriteText = screen.getByRole('heading', { name: /Favorite pokémons/i });
+    expect(favoriteText).toBeInTheDocument();
+  });
+});
