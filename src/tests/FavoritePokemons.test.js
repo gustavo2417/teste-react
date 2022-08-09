5987a2db1a91b7142d88b057fcc1e63702a5258e
Gustavo <gustavodarocha241@gmail.com>
2022-08-09 13:40:43 -0300

    requisitos 3 e 4 completos

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,31 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import renderWithRouter from './RenderWithRouter';
+import App from '../App';
+import { FavoritePokemons } from '../pages';
+
+describe('Testa a rota /favorites ', () => {
+  it('Teste se a pagina possui a mensagem esperada, caso não tenha nenhum favoritado',
+    () => {
+      renderWithRouter(<FavoritePokemons />);
+
+      const favorite = screen.getByText(/No favorite pokemon found/i);
+
+      expect(favorite).toBeInTheDocument();
+    });
+
+  it('Teste se a página contém as informações sobre a Pokédex', () => {
+    const { history } = renderWithRouter(<App />);
+
+    history.push('/pokemons/151');
+
+    const label = screen.getByLabelText(/Pokémon favoritado/i);
+    const favorite = screen.getByText(/favorite pokémons/i);
+    userEvent.click(label);
+    userEvent.click(favorite);
+    const pokemon = screen.getByText(/mew/i);
+
+    expect(pokemon).toBeInTheDocument();
+  });
+});
--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,23 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from './RenderWithRouter';
+import { NotFound } from '../pages';
+
+describe('Testando a pagina Not found', () => {
+  it('testa se é redirecionado para "/" ao clicar em home', () => {
+    renderWithRouter(<NotFound />);
+    const notFound = screen.getByText(/Page requested not found/i);
+
+    expect(notFound).toBeInTheDocument();
+  });
+
+  it('Testa se aparece a imaem esperada na  pagina Not Found', () => {
+    renderWithRouter(<NotFound />);
+
+    const
+      imgPikachu = screen
+        .getByAltText(/Pikachu crying because the page requested was not found/i);
+    const URL = 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif';
+    expect(imgPikachu).toHaveProperty('src', `${URL}`);
+  });
+});
